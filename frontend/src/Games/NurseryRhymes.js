6078d7661e827515e4ea82b3fcf6f35e39c47e57
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-03-07 20:44:32 -0500

    final errors in seed files fixed.

---
+++ frontend/src/Games/NurseryRhymes.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const NurseryRhymes = () => {
+  return (
+    <div>NurseryRhymes</div>
+  )
+}
+
+export default NurseryRhymes
+
+// [
+
+//     {
+//         "title": "ITSY BITSY SPIDER",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/w_lCi8U49mY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": false
+//     },
+//     {   "title": "OLD MACDONALD",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/dpnUYVezBVw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "5 Little Monkeys",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/0j6AZhZFb7A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title":"Bingo the dog",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/0IvC6fZYGMM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title":"If you're happy and you know it",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/LDP08F3op80" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Ants go marching",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/axPmoijvWKM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Hey Diddle Diddle",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/sJiw-edttDY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": false
+//     },
+//     {
+//         "title": "Wheels on the Bus",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/ki_R_4FcFK0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Hickory Dickeory Dock",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/yxoSyFkX_eA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Humpty Dumpty",
+//         "embed_video":<iframe width="560" height="315" src="https://www.youtube.com/embed/nrv495corBc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     }
+    
+    
+//     ]
--- frontend/src/pages/Storytime.js
+++ frontend/src/pages/Storytime.js
@@ -8,3 +8,58 @@ const Storytime = () => {
 }
 
 export default Storytime
+
+// [
+
+//     {
+//         "title":"The Very Hungry Caterpillar",
+//         "embed_video":<iframe width="560" height="315" src="https://www.youtube.com/embed/75NQK-Sm1YY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": false
+//     },
+//     {
+//         "title": "The Cat in the Hat",
+//         "embed_video":<iframe width="560" height="315" src="https://www.youtube.com/embed/-LL62u6-OjY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> ,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Oh the Places You'll Go",
+//         "embed_video":<iframe width="560" height="315" src="https://www.youtube.com/embed/abw43kcLrbg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> ,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Little Red Riding Hood",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/DKdc_kVBz7c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "Goldilocks and the Three Bears",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/G_pUBlsgBZ8?start=19" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true
+//     },
+//     {
+//         "title": "The GingerBread Man",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/pckuS--UlV4?start=19" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true,
+//     },
+//     {
+//         "title": "The Three Little Pigs",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/W-WLnxbvMyw?start=19" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": false
+//     },
+//     {
+//         "title": "The Sour Grape",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/ZCL85YoRRJU?start=19" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true,
+//     },
+//     {
+//         "title": "We Don't eat our Classmates",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/ff1THe0tItY?start=19" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true,
+//     },
+//     {
+//         "title": "The Rainbow Fish",
+//         "embed_video": <iframe width="560" height="315" src="https://www.youtube.com/embed/TOnuVm4OrCc?start=19" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+//         "includes_lyrics": true,
+//     }
+    
+//     ]
